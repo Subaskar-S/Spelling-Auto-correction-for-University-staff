c19a3625823a4a851c31c84efd46c81bdf59ab36
Subaskar-S <87691395+Subaskar-S@users.noreply.github.com>
2023-12-11 01:28:16 +0530

    v1

--- app.py
+++ app.py
@@ -1,74 +1,53 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request, render_template
+from textdistance import Jaccard
+from collections import Counter
 import pandas as pd
 import numpy as np
-import textdistance
 import re
-from collections import Counter
-import fitz  # PyMuPDF library for working with PDF files
-from flask_cors import CORS
+
+class SpellChecker:
+    def __init__(self, file_path='Data.txt'):
+        self.words = []
+        with open(file_path, 'r', encoding='utf-8') as f:
+            file_name_data = f.read()
+            file_name_data = file_name_data.lower()
+            self.words = re.findall('\w+', file_name_data)
+
+        self.V = set(self.words)
+        self.word_freq_dict = Counter(self.words)
+        self.Total = sum(self.word_freq_dict.values())
+        self.probs = {k: v / self.Total for k, v in self.word_freq_dict.items()}
+
+    def correct_spell(self, input_word):
+        input_word = input_word.lower()
+        if input_word in self.V:
+            return [input_word]  # Return a list containing the original word
+        else:
+            similarities = [1 - (Jaccard(qval=2).distance(v, input_word)) for v in self.word_freq_dict.keys()]
+            df = pd.DataFrame.from_dict(self.probs, orient='index').reset_index()
+            df = df.rename(columns={'index': 'Word', 0: 'Prob'})
+            df['Similarity'] = similarities
+            output = df.sort_values(['Similarity', 'Prob'], ascending=False).head()
+            return output['Word'].tolist()
 
 app = Flask(__name__)
+spell_checker = SpellChecker()
 
-def extract_text_from_pdf(pdf_path):
-    text = ""
-    with fitz.open(pdf_path) as doc:
-        num_pages = doc.page_count
-        for page_num in range(num_pages):
-            page = doc[page_num]
-            text += page.get_text()
+# routes
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-    return text.lower()
+@app.route('/spell', methods=['POST', 'GET'])
+def spell():
+    original_text = None
+    corrected_text = None
+    if request.method == 'POST':
+        original_text = request.form['text']
+        corrected_text = spell_checker.correct_spell(original_text)
 
-def preprocess_text(text):
-    return re.findall(r'\w+', text)
+    return render_template('index.html', original_text=original_text, corrected_text=corrected_text)
 
-def build_vocabulary(text):
-    return set(text)
-
-def build_word_frequency_dict(text):
-    return Counter(text)
-
-def calculate_word_probabilities(word_freq_dict):
-    total_words = sum(word_freq_dict.values())
-    probs = {k: v / total_words for k, v in word_freq_dict.items()}
-    return probs
-
-def my_autocorrect(input_word, vocab, probs, word_freq_dict):
-    input_word = input_word.lower()
-    if input_word in vocab:
-        return 'Your word seems to be correct'
-    else:
-        similarities = [1 - (textdistance.Jaccard(qval=2).distance(v, input_word)) for v in word_freq_dict.keys()]
-        df = pd.DataFrame.from_dict(probs, orient='index').reset_index()
-        df = df.rename(columns={'index': 'Word', 0: 'Prob'})
-        df['Similarity'] = similarities
-        output = df.sort_values(['Similarity', 'Prob'], ascending=False).head()
-        return output
-    
-@app.route('/autocorrect', methods=['POST'])
-def autocorrect():
-    data = request.get_json()
-    input_word = data['input_word']
-
-    # Call your autocorrect function here
-    result = my_autocorrect(input_word, V, probs, word_freq_dict)
-
-    return jsonify({'result': result.to_dict(orient='records')})
-
-
+# python main
 if __name__ == "__main__":
-    # Example usage:
-    pdf_path = r'C:\Users\Subaskar_S\AI\Datas\Data1.0.pdf'
-    pdf_text = extract_text_from_pdf(pdf_path)
-    preprocessed_text = preprocess_text(pdf_text)
-    V = build_vocabulary(preprocessed_text)
-    word_freq_dict = build_word_frequency_dict(preprocessed_text)
-    probs = calculate_word_probabilities(word_freq_dict)
-
-    # Test the autocorrect function
-    result = my_autocorrect('neverteless', V, probs, word_freq_dict)
-    print(result)
     app.run(debug=True)
-    CORS(app)
-
-
